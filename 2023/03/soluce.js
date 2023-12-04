f9be57db49a0a03a178769cac3996d50753288e1
Matthieu BRAULT <matthieu.brault1@gmail.com>
2023-12-04 14:06:42 +0100

    fix: day 3 part 2 2023

--- 2023/03/soluce.js
+++ 2023/03/soluce.js
@@ -43,14 +43,60 @@ for (let y = 0; y < map.length; y += 1) {
   }
 }
 
-`...5.5...
- ....*....`;
-
-`...55...
- ....*...`;
-
 console.log(
   'Part 1 soluce is',
-  partNumbers,
   _.sum(partNumbers),
+);
+
+const part2Start = process.hrtime();
+
+const numberPositions = [];
+const starPositions = [];
+
+for (let y = 0; y < map.length; y += 1) {
+  let number = '';
+  let positionFrom = null;
+  for (let x = 0; x < map[y].length; x += 1) {
+    if (map[y][x] >= '0' && map[y][x] <= '9') {
+      number += map[y][x];
+      if (positionFrom === null) positionFrom = x;
+      if (x === map[y].length - 1) {
+        numberPositions.push({
+          number: Number(number), y, from: positionFrom, to: x,
+        });
+        number = '';
+        positionFrom = null;
+      }
+    } else if (number !== '') {
+      numberPositions.push({
+        number: Number(number), y, from: positionFrom, to: x - 1,
+      });
+      number = '';
+      positionFrom = null;
+    }
+    if (map[y][x] === '*') {
+      starPositions.push({ y, x });
+    }
+  }
+}
+
+const res = _.chain(starPositions)
+  .map(({ x, y }) => {
+    const numbers = numberPositions.filter((n) => {
+      if (![y - 1, y, y + 1].includes(n.y)) return false;
+      const xRanges = _.range(n.from, n.to + 1);
+      return [x - 1, x, x + 1].some((xPos) => xRanges.includes(xPos));
+    });
+    return numbers.length > 1 ? numbers.reduce((acc, value) => (acc || 1) * value.number, 0) : 0;
+  })
+  .sum()
+  .value();
+
+const part2EndTime = process.hrtime(part2Start);
+const duration = part2EndTime[0] * 1000 + part2EndTime[1] / 1e6;
+
+console.log(
+  'Part 2 soluce is',
+  res,
+  `in ${duration}ms`,
 );
